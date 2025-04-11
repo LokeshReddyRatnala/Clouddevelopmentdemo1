72f689a5a69c5857852583e18c6e0d8567730a69
LokeshReddyRatnala <lokeshreddyratnala86@gmail.com>
2025-04-10 20:48:25 -0500

    Added package

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 // MySQL connection config
 // triggering azure deployment
 const db = mysql.createConnection({
-  host: 'studentdb-demo.mysql.database.azure.com',
-  user: 'mysql_admin',
-  password: 'TempPass@123',
+  host: 'cloudbackendserver.mysql.database.azure.com',
+  user: 'loki',
+  password: 'Lokesh@123',
   database: 'studentdb',
   port: 3306,
   ssl: {
@@ -31,5 +31,5 @@ app.get('/api/students', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
